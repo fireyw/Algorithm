b604bbe1c75593db92d72f6b59799c7fdc0f8608
fireyw <fireyw@naver.com>
2016-07-18 16:47:43 +0900

    1463 문제 풀이, 재귀함수 기본, 메모이제이션

--- Algorithm/src/p1463.java
+++ Algorithm/src/p1463.java
@@ -7,8 +7,30 @@
 //연산을 사용하는 횟수의 최소값을 출력하시오.
 //10의 경우에 10 -> 9 -> 3 -> 1 로 3번 만에 만들 수 있다.
 
+import java.util.Scanner;
+
 public class p1463 {
-	public static void main(String args){
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
 		
+		int n= s.nextInt();
+		int count =0;
+		while(n!=1){
+			if(n%3==0){
+				n=n/3;
+				count++;
+			}else if((n-1)%3==0){
+				n=n-1;
+				count++;
+			}else if(n%2==0){
+				n=n/2;
+				count++;
+			}else{
+				n=n-1;
+				count++;
+			}
+		}
+		System.out.println(count);
+		s.close();
 	}
 }
