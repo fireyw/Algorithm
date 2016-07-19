1170da528d996f4124a9723dbf22d63746c8bdc5
fireyw <fireyw@naver.com>
2016-07-19 16:22:28 +0900

    BufferedReader를 사용한 시간 단축

--- Algorithm/src/p1000.java
+++ Algorithm/src/p1000.java
@@ -1,17 +1,22 @@
 //두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class p1000 {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		//int a=Integer.parseInt(s.next());
-		//int b=Integer.parseInt(s.next());
+		/*Scanner s = new Scanner(System.in);
 		int a= s.nextInt();
-		int b= s.nextInt();
-		System.out.println(a+b);
+		int b= s.nextInt();*/
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 		
+		StringTokenizer token = new StringTokenizer(read.readLine());
+		
+		int a= Integer.parseInt(token.nextToken());
+		int b= Integer.parseInt(token.nextToken());
+		
+		System.out.println(a+b);	
 	}
-
 }
--- Algorithm/src/p9465.java
+++ Algorithm/src/p9465.java
@@ -1,19 +1,62 @@
 
 //https://www.acmicpc.net/problem/9465 문제참조
 //현재 문제 분석중
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class p9465 {
-	public static void main(String[] args){
-		Scanner s = new Scanner(System.in);
-		int count= s.nextInt();
-		
-		long max=0;
-		long temp=0;
-		int[] a = new int[count];
-		for(int i=0;i<count;i++){
-			a[i]=s.nextInt();
-		}
+	static int count=0;
+	static int ret =0;
+	static int arr[][];
+	static int memo[][]=new int[2][100000];
+	public static void main(String[] args) throws NumberFormatException, IOException{
 
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+	    //BufferedReader의 경우 엔터를 하나로 인식, 엔터가 있을 경우 token으로 쪼개줘야함
+	    int t = Integer.parseInt(reader.readLine());
+		
+		int maxArr[] = new int[t];
+		
+		StringTokenizer token = new StringTokenizer("");
+		
+		for(int a=0; a<t; a++){
+			count = Integer.parseInt(reader.readLine());
+			arr= new int[2][count];
+			
+			for(int i=0;i<2;i++){  //메모이제이션 -1로 초기화
+				for(int j=0;j<100000;j++){
+					memo[i][j] = -1;
+				}
+			}
+			
+			for(int i=0;i<2;i++){
+				token = new StringTokenizer(reader.readLine());
+				for(int j=0;j<count;j++){
+					arr[i][j]= Integer.parseInt(token.nextToken());
+				}
+			}
+			maxArr[a]= Math.max(sol(0, 0), sol(1, 0));
+		}
+		
+		for(int a=0; a<t; a++){
+			System.out.println(maxArr[a]);
+		}
+	}
+	
+	static public int sol(int a, int b){
+		if(b>=count)
+			return 0;
+		if(memo[a][b]!=-1){
+			return memo[a][b];
+		}
+		else if(a==0){
+			ret=Math.max(sol(1,b+1), sol(1, b+2))+arr[a][b];
+		}else{
+			ret=Math.max(sol(0,b+1), sol(1, b+2))+arr[a][b];
+		}
+		memo[a][b] = ret;
+		return ret;
 	}
 }
