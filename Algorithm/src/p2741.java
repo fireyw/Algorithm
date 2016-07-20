9170d0a48702398e54a71e9e74f516790a95309d
fireyw <fireyw@naver.com>
2016-07-20 10:56:15 +0900

    재귀함수 변경 및 BufferedReader 사용

---
+++ Algorithm/src/p2741.java
@@ -0,0 +1,16 @@
+//두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2741 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(read.readLine());
+		for(int i=1;i<=a; i ++)
+		System.out.println(i);
+
+	}
+}
