3b886a118fc44502fc7898619d423221b1951385
fireyw <fireyw@naver.com>
2016-07-20 18:26:52 +0900

    2096내려가기 문제 중간 commit

--- Algorithm/src/p2096.java
+++ Algorithm/src/p2096.java
@@ -8,15 +8,28 @@ import java.util.StringTokenizer;
 //먼저 처음에 적혀 있는 세 개의 숫자 중에서 하나를 골라서 시작하게 된다. 그리고 다음 줄로 내려가는데, 다음 줄로 내려갈 때에는 다음과 같은 제약 조건이 있다.
 //바로 아래의 수로 넘어가거나, 아니면 바로 아래의 수와 붙어 있는 수로만 이동할 수 있다는 것이다. 이 제약 조건을 그림으로 나타내어 보면 다음과 같다.
 //https://www.acmicpc.net/problem/2096
+//동적으로 할당해서 수를 계속 비교하는 방법으로 풀이
 public class p2096 {
+	static int arr[][];
 	public static void main(String[] args) throws IOException{
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    String input = reader.readLine();
 
-	    double number = Double.parseDouble(input);
+	    int number = Integer.parseInt(input);
+	    
+	    arr = new int[number][3];
 
-	    System.out.println(input + ":" + Math.sqrt(number));
-
+	    StringTokenizer token = new StringTokenizer("");
+	    
+	    
+	    for(int i=0; i<number; i++){
+	    	token=new StringTokenizer(reader.readLine());
+	    	for(int j=0; j<3;j++){
+	    		arr[i][j] = Integer.parseInt(token.nextToken());
+	    	}
+	    	System.out.println(i+ " 번째줄입력완료");
+	    }
+	    System.out.println("종료");
 	    reader.close();
 	}
 }
