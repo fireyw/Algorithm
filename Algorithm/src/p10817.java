4576ad4539274027d32be9dcee961118ad28ca2e
fireyw <fireyw@naver.com>
2016-07-27 09:28:09 +0900

    세 수 알고리즘 문제 추가

---
+++ Algorithm/src/p10817.java
@@ -0,0 +1,11 @@
+//세 정수 A, B, C가 주어진다. 이 때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. 
+//20 30 10 -->20
+//40 40 40-->40
+public class p10817 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
